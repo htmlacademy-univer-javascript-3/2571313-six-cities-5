bab4a90d9b2705a7dea4b83ec77260d8fa62e7f6
Aftenius <82750159+Aftenius@users.noreply.github.com>
2025-03-11 12:07:10 +0300

    добавил карточки и настроил роутинг

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+import { offersMock } from './mocks/offers.ts';
 
 const root = ReactDOM.createRoot(
-   document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
-    <App></App>
+    <App offers={offersMock}></App>
   </React.StrictMode>
 );
